0c2a9d757975291b862e027cb2f827c85d275009
RenataArdito <10417520@mackenzista.com.br>
2025-05-09 10:28:09 -0300

    Commit inicialização

---
+++ src/main/java/cloud9/bookcloud/BookcloudApplication.java
@@ -0,0 +1,13 @@
+package cloud9.bookcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookcloudApplication.class, args);
+	}
+
+}
